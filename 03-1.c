eb889157a52e2ad82c1a2d773ba4d896dbceb48a
Nurs0106 <saifudinov01@gmail.com>
2021-02-01 15:45:59 +0300

    Lab 3 done.

--- 03-1.c
+++ 03-1.c
@@ -6,21 +6,14 @@ int main()
 {
   pid_t pid, ppid;
   int a = 0;
-
-  fork();
-  //
-  // If the new process is created successfully,
-  // two processes start working in pseudo-parallel from this point: the old and the new one.
-  //
-  // Before the following expression is executed,
-  // the value of the variable a in both processes is 0.
-  //
-  a = a+1;
-
+  while(a < 7){
+    fork();
+    a = a+1;
+  }
   pid  = getpid();
   ppid = getppid();
 
-  printf("My pid = %d, my ppid = %d, result = %d\n", (int)pid, (int)ppid, a);
+  printf("Current pid = %d; current ppid = %d; outcome = %d\n", (int)pid, (int)ppid, a);
 
   return 0;
 }
