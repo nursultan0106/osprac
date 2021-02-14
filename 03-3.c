7b28b9b170971064f8d8059085c2aa1fcced8619
Nurs0106 <saifudinov01@gmail.com>
2021-02-14 15:08:25 +0300

    Lab 3 remake of the incorrect task 3

--- 03-3.c
+++ 03-3.c
@@ -1,21 +1,19 @@
+#include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdio.h>
 
-int main(int argc, char *argv[], char *envp[])
+int main(int argc, char* argv[], char* envp[])
 {
-printf("Argc = %d", argc);
-for(int i = 0; i < argc; i++)
-{
-printf("The %d element of argv array = ", i);
-puts(argv[i]);
-printf("\n");
-}
-for(int i = 0; i < argc; i++)
-{
-printf("The %d element of envp array = ", i);
-puts(envp[i]);
-printf("\n");
-}
-return 0;
+    printf("Arguments of the command line:\n");
+    for (int i = 0; i < argc; ++i)
+        printf("%s\n", argv[i]);
+    printf("\n");
+
+    printf("Parametres of the environment:\n");
+    int i = 0;
+    while (envp[i] != NULL)
+        printf("%s\n", envp[i++]);
+    printf("\n");
+
+    return 0;
 }
