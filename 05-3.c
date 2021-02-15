6d798320da8c3a8329c31babf7b12c2cd6600dc8
Nurs0106 <saifudinov01@gmail.com>
2021-02-15 15:21:23 +0300

    Lab 5 done

--- 05-3.c
+++ 05-3.c
@@ -5,63 +5,61 @@
 
 int main()
 {
-  int     fd[2], result;
+    printf("Start of the program\n");
+    int pipe_parent[2], pipe_child[2], answer;
+    size_t size;
+    char answer_string[14];
 
-  size_t size;
-  char  resstring[14];
-
-  if (pipe(fd) < 0) {
-    printf("Can\'t open pipe\n");
-    exit(-1);
-  }
-
-  result = fork();
-
-  if (result < 0) {
-    printf("Can\'t fork child\n");
-    exit(-1);
-  } else if (result > 0) {
-
-   /* Parent process */
-
-    if (close(fd[0]) < 0) {
-      printf("parent: Can\'t close reading side of pipe\n"); exit(-1);
+    if (pipe(pipe_parent) < 0) {
+        printf("Can not create the parent pipe\n");
+        exit(-1);
     }
 
-    size = write(fd[1], "Hello, world!", 14);
-
-    if (size != 14) {
-      printf("Can\'t write all string to pipe\n");
-      exit(-1);
+    if (pipe(pipe_child) < 0) {
+        printf("Can not create the child pipe\n");
+        exit(-1);
     }
 
-    if (close(fd[1]) < 0) {
-      printf("parent: Can\'t close writing side of pipe\n"); exit(-1);
+    answer = fork();
+    if (answer < 0) {
+        printf("Can not fork the child\n");
+        exit(-1);
+    } else if (answer > 0) {
+        close(pipe_parent[0]);
+        close(pipe_child[1]);
+        size = write(pipe_parent[1], "Hello, world!", 14);
+        if (size != 14) {
+            printf("Can not write all strings\n");
+            exit(-1);
+        }
+        close(pipe_parent[1]);
+
+        size = read(pipe_child[0], answer_string, 14);
+        if (size != 14) {
+            printf("Can not read from the child\n");
+            exit(-1);
+        }
+        printf("Parent pulled from the child out: %s\n", answer_string);
+        printf("Parent exit\n");
+    } else {
+        close(pipe_parent[1]);
+        close(pipe_child[0]);
+        size = read(pipe_parent[0], answer_string, 14);
+        if (size < 0) {
+            printf("Can not get from the parent\n");
+            exit(-1);
+        }
+        printf("Pulled from the parent out: %s\n", answer_string);
+	//Remake the string to make it more clear that we have 2 different options with "Hello world" string 
+	int x = 13;
+        for (int i = 0; i < x / 2; i++) {
+        char t = answer_string[i];
+        answer_string[i] = answer_string[x - i - 1];
+        answer_string[x - i - 1] = t;
+        }
+        size = write(pipe_child[1], answer_string, 14);
+        close(pipe_parent[0]);
+        printf("Child exit\n");
     }
-
-    printf("Parent exit\n");
-
-  } else {
-
-    /* Child process */
-
-    if (close(fd[1]) < 0) {
-      printf("child: Can\'t close writing side of pipe\n"); exit(-1);
-    }
-
-    size = read(fd[0], resstring, 14);
-
-    if (size < 0) {
-      printf("Can\'t read string from pipe\n");
-      exit(-1);
-    }
-
-    printf("Child exit, resstring:%s\n", resstring);
-
-    if (close(fd[0]) < 0) {
-      printf("child: Can\'t close reading side of pipe\n"); exit(-1);
-    }
-  }
-
-  return 0;
+    return 0;
 }
