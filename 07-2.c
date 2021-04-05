8b716a9bca3ee1a446543435b66f97d59be3193e
Nurs0106 <saifudinov01@gmail.com>
2021-04-05 15:40:08 +0300

    Lab 7 task 2 done

--- 07-2.c
+++ 07-2.c
@@ -2,64 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int a = 0;  // The variable a is global static for the entire program,
-            // so it will be shared by both threads of execution.
+int a = 0;
 
-/**
-  Below follows the text of the function
-  that will be associated with the 2nd thread.
-
-  Parameter dummy is used only for type compatibility.
-  For the same reason, the function returns void *.
-**/
 void *mythread(void *dummy)
 {
-  pthread_t mythid;
-  //
-  // Note that mythid variable is a dynamic local variable of mythread () function,
-  // that is, it is pushed on the stack and, therefore, is not shared by the threads.
-  //
-  mythid = pthread_self();
-
-  a = a+1;
-  printf("Thread %u, Calculation result = %d\n", mythid, a);
-  return NULL;
+    pthread_t mythid;
+    mythid = pthread_self();
+    a = a + 1;
+    printf("Thread %d, Calculation result = %d\n", mythid, a);
+    return NULL;
 }
 
-/**
-  The main() function is also an associated function of the main thread.
-**/
 int main()
 {
-  pthread_t thid, mythid;
-  int       result;
-  //
-  // Try to create a new thread of execution associated with the mythread () function.
-  // Pass it NULL as a parameter. If it succeeds,
-  // the identifier of the new thread is written into the thid variable.
-  // If an error occurs, stop working.
-  //
-  result = pthread_create (&thid, (pthread_attr_t *)NULL, mythread, NULL);
+    pthread_t thid, another_thid, mythid;
+    int result;
 
-  if (result != 0) {
-    printf ("Error on thread create, return value = %d\n", result);
-    exit(-1);
-  }
+    result = pthread_create( &thid, (pthread_attr_t *)NULL, mythread, NULL);
 
-  printf("Thread created, thid = %u\n", thid);
+    if (result != 0) {
+        printf ("Error on thread create, return value = %d\n", result);
+        exit(-1);
+    }
 
-  mythid = pthread_self();
+    printf("Second thread created, thid = %d\n", thid);
 
-  a = a+1;
+    result = pthread_create( &another_thid, (pthread_attr_t *)NULL, mythread, NULL);
 
-  printf("Thread %u, Calculation result = %d\n", mythid, a);
-  //
-  // Wait for the spawned thread to terminate, not caring what value it returns.
-  // If this function is not called, the main() function may end before
-  // the spawned thread is executed, which will automatically cause it to terminate,
-  // distorting the results.
-  //
-  pthread_join(thid, (void **)NULL);
+    if (result != 0) {
+        printf ("Error on thread create, return value = %d\n", result);
+        exit(-1);
+    }
 
-  return 0;
+    printf("Third thread created, thid = %d\n", another_thid);
+
+    mythid = pthread_self();
+    a = a + 1;
+    printf("Thread %d, Calculation result = %d\n", mythid, a);
+
+    pthread_join(thid, (void **)NULL);
+    pthread_join(another_thid, (void **)NULL);
+
+    return 0;
 }
